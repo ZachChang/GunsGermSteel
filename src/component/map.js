a8ffb8948cd6574c07fbb479d35330db0faaf182
Zach <podeng0321@gmail.com>
2019-09-09 18:17:07 +0800

    0909

--- src/component/map.js
+++ src/component/map.js
@@ -10,11 +10,13 @@ import {
 import { worldMap } from '../static/mapJson';
 import { sankeyCoordinates } from '../static/sankeyData';
 import HumanFateSankey from './chart';
-import * as d3 from 'd3'
+import * as d3 from 'd3';
+import debounce from 'lodash.debounce';
 
 const wrapperStyles = {
   width: 980,
-  margin: '0 auto'
+  // margin: '0 auto',
+  height: 2000
 }
 
 const mapStyle = {
@@ -28,6 +30,13 @@ class Map extends Component {
   constructor() {
     super();
     this.state = {
+      scrollY: 0,
+      step: 0,
+
+      barPostions: [],
+
+      pathVisibility: 'hidden',
+
       lineMarker: {},
       sankeyData: null,
       sankeyWidth: 40,
@@ -39,12 +48,44 @@ class Map extends Component {
       },
       worldMapStyle: {
         width: '100%',
-        transform: 'translateX(-100px)',
+        transform: 'translateX(-200px) translateY(-100px)',
         transition: 'transform 1s ease-in-out, box-shadow 1s ease-in-out'
       }
     };
+
+    this.onMapScroll = this.onMapScroll.bind(this);
+    this._handleScroll = debounce(this._handleScroll, 3000, {
+      'leading': true,
+      'trailing': false
+    });
   }
-  next = () => {
+
+  _findPostion = () => {
+    console.log('_findPostion');
+    const markersPostion = [];
+    for (let i = 0; i < sankeyCoordinates.length; i++) {
+      //find each marker's starting point on the map to set bar chart
+      let markerRect = document.getElementById(sankeyCoordinates[i].continent).getBoundingClientRect();
+      let postion = {
+        x: markerRect.left,
+        y: markerRect.top + markerRect.height,
+        width: markerRect.width
+      };
+      markersPostion.push(postion);
+    }
+    console.log({markersPostion});
+
+    this._drawBarChart(markersPostion);
+  }
+
+  _drawBarChart = (postions) => {
+    console.log('draw');
+    this.setState({
+      barPostions: postions
+    })
+  }
+
+  roateMap = () => {
     const prevMarker = this.state.sankeyMarker;
     const prevWorldMap = this.state.worldMapStyle;
     if (this.state.rotate) {
@@ -53,20 +94,13 @@ class Map extends Component {
         rotate: false,
         worldMapStyle: {
           ...prevWorldMap,
-          transform: 'translateX(-100px)',
+          transform: 'translateX(-200px) translateY(-100px)',
         },
         sankeyMarker: {
           ...prevMarker,
           transform: 'rotate(-90deg) translateY(300px)',
         }
       });
-
-      setTimeout(
-        d3.json('/first-sankey-data.json').then(data =>
-          this.setState({
-            sankeyData: data,
-          }))
-        ,2500);
     }
    else {
      this.setState({
@@ -74,7 +108,7 @@ class Map extends Component {
        rotate: true,
        worldMapStyle: {
          ...prevWorldMap,
-         transform: 'perspective(600px) rotateX(45deg) translateX(-100px)'
+         transform: 'perspective(600px) rotateX(45deg) translateX(-200px) translateY(-100px)'
        },
        sankeyMarker: {
          ...prevMarker,
@@ -82,71 +116,135 @@ class Map extends Component {
        }
      })
 
-     setTimeout(
-       d3.json('/main-sankey-data.json').then(data =>
-         this.setState({
-           sankeyData: data,
-         }))
-       ,2500);
-
-
+     setTimeout(() => {
+       this._findPostion();
+     }, 1500);
    }
   }
+
+  _handleScroll = () => {
+    const { scrollY, step } = this.state;
+    const currentScrollY = window.scrollY;
+    const forwoard = currentScrollY > scrollY ? true : false;
+
+    if (true) {
+      if (step===0) {
+        this.setState({pathVisibility: 'visible'});
+      } else if (step===1) {
+        this.roateMap();
+        this.setState({pathVisibility: 'hidden'});
+      }
+      this.setState({
+        step: step + 1,
+        scrollY: currentScrollY
+      })
+    } else {
+      this.setState({
+        step: step - 1,
+        scrollY: currentScrollY
+      })
+    }
+    console.log('onMapScroll', {scrollY, currentScrollY, forwoard, step});
+  }
+
+  onMapScroll(e){
+    console.log('onMapScroll');
+    this._handleScroll();
+  }
   componentDidMount() {
+    window.addEventListener('scroll', this.onMapScroll, true);
+
     d3.json('/first-sankey-data.json').then(data =>
       this.setState({sankeyData: data})
     )
   }
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.onMapScroll, true);
+  }
   render() {
-    const { sankeyMarker, lineMarker, worldMapStyle, sankeyData, sankeyWidth, showSankey } = this.state;
+    const { sankeyMarker,
+      lineMarker,
+      worldMapStyle,
+      sankeyData,
+      sankeyWidth,
+      showSankey,
+      barPostions,
+      pathVisibility,
+      step
+    } = this.state;
     return (
-      <div style={wrapperStyles}>
-        <div onClick={this.next}>
-          BUTTON
-        </div>
-        <ComposableMap
-          projectionConfig={{
-            scale: 205,
-            rotation: [-11,0,0],
-          }}
-          width={980}
-          height={800}
-          style={worldMapStyle}
-          >
-          <ZoomableGroup center={[0,20]} disablePanning>
-            <Geographies geography={worldMap}>
-              {(geographies, projection) => geographies.map((geography, i) => geography.id !== "ATA" && (
-                <Geography
-                  key={i}
-                  geography={geography}
-                  projection={projection}
-                  style={{
-                    default: mapStyle,
-                    hover: mapStyle,
-                    pressed: mapStyle
-                  }}
-                />
-              ))}
-            </Geographies>
-            <Markers style={showSankey ? sankeyMarker : lineMarker}>
-              {sankeyCoordinates.map((continent, i) =>
-                <Marker
-                  key={i}
-                  marker={continent}
-                >
-                  <rect
-                    width={continent.width}
-                    height={150}
-                    y={-150}
+      <div style={wrapperStyles} onScroll={this.onMapScroll}>
+        <div className='fixed-container'>
+          {step===2 &&
+            barPostions.map((postion, i) =>
+            // to set the bar chart height grow from bottom to top, we have to set the child div bottom, so it has nowhere to vertically increase but upwards
+              <div
+                key={i}
+                style={{
+                  position: 'absolute',
+                  zIndox: '10',
+                  left: postion.x - 8 , //reduce the edge width of the body
+                  top: postion.y - 8 , //reduce the edge width of the body
+                  width: postion.width
+                }}
+              >
+                <div style={{ position: 'relative'}}>
+                  <div
                     style={{
-                      fill: 'F78A64',
+                      bottom: '0',
+                      width: postion.width
+                    }}
+                    className={`barchart animate-${i}`}
+                  ></div>
+                </div>
+              </div>
+            )
+          }
+          <ComposableMap
+            projectionConfig={{
+              scale: 205,
+              rotation: [-11,0,0],
+            }}
+            width={980}
+            height={800}
+            style={worldMapStyle}
+            >
+            <ZoomableGroup center={[0,20]} disablePanning>
+              <Geographies geography={worldMap}>
+                {(geographies, projection) => geographies.map((geography, i) => geography.id !== "ATA" && (
+                  <Geography
+                    key={i}
+                    geography={geography}
+                    projection={projection}
+                    style={{
+                      default: mapStyle,
+                      hover: mapStyle,
+                      pressed: mapStyle
                     }}
                   />
-                </Marker>
-              )}
-            </Markers>
-          </ZoomableGroup>
-        </ComposableMap>
+                ))}
+              </Geographies>
+                <Markers style={showSankey ? sankeyMarker : lineMarker}>
+                  {sankeyCoordinates.map((continent, i) =>
+                    <Marker
+                      key={i}
+                      marker={continent}
+                    >
+                      <rect
+                        id={continent.continent}
+                        width={continent.width}
+                        height={2}
+                        style={{
+                          fill: 'F78A64',
+                          visibility: pathVisibility
+                        }}
+                      />
+                    </Marker>
+                  )}
+                </Markers>
+            </ZoomableGroup>
+          </ComposableMap>
+        </div>
       </div>
     )
   }
