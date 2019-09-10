b38fd5309bccbbd88c42e6baedb03f65a5aa86fb
Zach <podeng0321@gmail.com>
2019-09-10 12:31:00 +0800

    Updates

---
+++ precache-manifest.f34e102d3a01c6ae933b5559c84adbcb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d334587cefd4da47f74562d288cf470e",
+    "url": "/GunsGermSteel/index.html"
+  },
+  {
+    "revision": "48fa992de41f001885ee",
+    "url": "/GunsGermSteel/static/css/2.7b2fcd39.chunk.css"
+  },
+  {
+    "revision": "61070f09b439859bbaf1",
+    "url": "/GunsGermSteel/static/css/main.2344c97e.chunk.css"
+  },
+  {
+    "revision": "48fa992de41f001885ee",
+    "url": "/GunsGermSteel/static/js/2.2e4a76c4.chunk.js"
+  },
+  {
+    "revision": "61070f09b439859bbaf1",
+    "url": "/GunsGermSteel/static/js/main.a94d0f5b.chunk.js"
+  },
+  {
+    "revision": "d5446f15a1746e5f7b64",
+    "url": "/GunsGermSteel/static/js/runtime~main.78082ed0.js"
+  },
+  {
+    "revision": "0cb0b9c589c0624c9c78dd3d83e946f6",
+    "url": "/GunsGermSteel/static/media/Simple-Line-Icons.0cb0b9c5.woff2"
+  },
+  {
+    "revision": "2fe2efe63441d830b1acd106c1fe8734",
+    "url": "/GunsGermSteel/static/media/Simple-Line-Icons.2fe2efe6.svg"
+  },
+  {
+    "revision": "78f07e2c2a535c26ef21d95e41bd7175",
+    "url": "/GunsGermSteel/static/media/Simple-Line-Icons.78f07e2c.woff"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/GunsGermSteel/static/media/Simple-Line-Icons.d2285965.ttf"
+  },
+  {
+    "revision": "f33df365d6d0255b586f2920355e94d7",
+    "url": "/GunsGermSteel/static/media/Simple-Line-Icons.f33df365.eot"
+  }
+]);
